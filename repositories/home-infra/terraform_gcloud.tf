0e6434a7b183ff02cdf299f4232731bb01915b0e
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-12-09 20:40:08 +0100

    feat(infra): add repos

---
+++ repositories/home-infra/terraform_gcloud.tf
@@ -0,0 +1,41 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TERRAFORM GCLOUD
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#tfsec:ignore:github-repositories-enable_vulnerability_alerts
+module "terraform_gcloud" {
+  source  = "mineiros-io/repository/github"
+  version = "0.16.2"
+
+  name         = "terraform-gcloud"
+  description  = ""
+  homepage_url = ""
+
+  # select template repo
+  # https://github.com/jazzlyn/repo-template-basic
+  # https://github.com/jazzlyn/repo-template-terraform
+  template = {
+    owner      = "jazzlyn"
+    repository = "repo-template-terraform"
+  }
+  auto_init = false
+
+  extra_topics = [
+    "terraform",
+  ]
+
+  defaults             = var.defaults.private
+  visibility           = "private"
+  has_downloads        = false # required to create static build assets on release f.e.
+  archived             = false
+  archive_on_destroy   = true
+  vulnerability_alerts = false
+
+  # individual collaborators
+  # pull_collaborators = []
+  # triage_collaborators = []
+  # push_collaborators = distinct(concat(
+  #   [for user in var.collaborators["group_name"] : user],
+  #   ["some_github_user"],
+  # ))
+}
