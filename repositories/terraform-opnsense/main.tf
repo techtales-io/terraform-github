d6bccd5ef15666a3505a3a9c79363f89085f80eb
Nils Müller <tyriis@users.noreply.github.com>
2025-03-02 01:13:01 +0100

    repo(terraform-opnsense): create

---
+++ repositories/terraform-opnsense/main.tf
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------------
+# CONFIGURE TERRAFORM
+# --------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 1.5.0, <= 1.11.0"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.6.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "1.1.1"
+    }
+  }
+}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.sops.yaml"
+}
