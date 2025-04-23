6dbd3b16ed1d5f57dd992a64937e680cb0f3e441
Nils Müller <tyriis@users.noreply.github.com>
2025-04-23 22:51:33 +0200

    feat(terraform-discord): implemented new atlantis webhook
    - configure trivy
    - update mise tools

--- repositories/backstage-docs/branches.tf
+++ repositories/backstage-docs/branches.tf
@@ -22,7 +22,7 @@ resource "github_branch_protection" "default" {
   allows_force_pushes             = var.branch_protections[github_branch_default.repo.branch].allows_force_pushes
   enforce_admins                  = var.branch_protections[github_branch_default.repo.branch].enforce_admins
   require_conversation_resolution = var.branch_protections[github_branch_default.repo.branch].require_conversation_resolution
-  require_signed_commits          = var.branch_protections[github_branch_default.repo.branch].require_signed_commits
+  require_signed_commits          = var.branch_protections[github_branch_default.repo.branch].require_signed_commits #trivy:ignore:AVD-GIT-0004 #trivy:ignore:AVD-GIT-0004
 
   # required_pull_request_reviews {
   #   dismiss_stale_reviews           = var.branch_protections[github_branch_default.repo.branch].required_pull_request_reviews.dismiss_stale_reviews
